a5e0448a96e6dbc6b88c7454f776233a84c7a899
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-06-15 14:55:52 +0300

    up

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -14,8 +14,8 @@ const AppRoutes = () => {
         <Route index element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
         <Route path="favorites" element={<FavoritesPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
       </Route>
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
